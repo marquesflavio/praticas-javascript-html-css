dc60f43421ed3841047580eeaf84b555026fcb62
Flávio Marques <121177356+marquesflavio@users.noreply.github.com>
2023-02-07 23:12:59 -0300

    exercícios array

--- array/for.js
+++ array/for.js
@@ -1,5 +1,5 @@
 const numeros = [100, 200, 300, 400, 500, 600]
 
-for(c = 0; c < numeros.length; c++){
-    console.log(numeros[c])
+for(i = 0; i < numeros.length; i++){
+    console.log(numeros[i])
 }
