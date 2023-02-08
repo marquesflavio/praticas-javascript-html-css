06b4d360acf8045ddc1ab70d97216cd12a33961f
Flávio Marques <121177356+marquesflavio@users.noreply.github.com>
2023-02-07 22:42:48 -0300

    Ex de array

---
+++ array/for.js
@@ -0,0 +1,5 @@
+const numeros = [100, 200, 300, 400, 500, 600]
+
+for(c = 0; c < numeros.length; c++){
+    console.log(numeros[c])
+}
--- array/nome-na-lista.js
+++ array/nome-na-lista.js
@@ -7,9 +7,13 @@ const listaAlunosENotas = [alunos,notas]
 function mostraNomeENota(aluno){
     if(listaAlunosENotas[0].includes(aluno)){ //se o array[0] do array listaAlunosENotas incluir o aluno...
         console.log(`${aluno} está cadastrado(a)!`)
+        //const alunos = listaAlunosENotas[0]
+        //const medias = listaAlunosENotas[1]
+        const [alunos,medias] = listaAlunosENotas //forma nova de atribuir const a partir de um mesmo local. (desestruturação de lista)
 
-        const indice = listaAlunosENotas[0].indexOf(aluno) //retorna o índice do aluno no array[0] do array listaAlunosENotas 
-        console.log(`Seu índice é o: ${indice}`)
+        const indice = alunos.indexOf(aluno) //retorna o índice do aluno no array[0] do array listaAlunosENotas 
+        const media = medias[indice]
+        console.log(`${aluno} está cadastrado(a) e a sua média é: ${media}`)
 
     } else{
         console.log(`${aluno} não está cadastrado!`)
